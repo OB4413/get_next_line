f381b0dbd34925bfa9d5166534ea20d04343f647
Ousama Barais <obarais@e1r12p12.1337.ma>
2024-11-09 12:41:39 +0100

    pub

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obarais <obarais@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 11:44:36 by obarais           #+#    #+#             */
+/*   Updated: 2024/11/09 12:35:17 by obarais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+
+#endif
